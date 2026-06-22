f6f8ba8990fcebf423c41258bff7879b0a5c812c
Istiak Hasan <istiakhasan1996@gmail.com>
2026-06-22 17:30:09 +0600

    abc

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: ['https://cbmnp-frontend-nu.vercel.app', 'http://localhost:3000','http://localhost:3001','https://www.nonggor.com','https://nonggor.com','https://erp.fitone.cloud'],
+    origin: ['https://cbmnp-frontend-nu.vercel.app', 'http://localhost:3000','http://localhost:3001','https://www.nonggor.com','https://nonggor.com','https://erp.curefit.bd'],
     methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH'],
     credentials: true,
     allowedHeaders: [
